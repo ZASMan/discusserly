0718df9b874cbada6bd5a1b2b707571dff740b30
zane <i.zane.am.robot@gmail.com>
2016-11-27 22:23:42 -0500

    Small params fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,7 +73,7 @@ class UsersController < ApplicationController
 	private
 
 		def user_params
-			params.require(:user).permit(:email, :password, :password_confirmation)
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
 
 		#For redirecting users who are already authenticated to their own profile page
