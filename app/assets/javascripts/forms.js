9391e258ecb676d9600a822cff67302677f14531
zane <i.zane.am.robot@gmail.com>
2016-11-28 22:22:36 -0500

    stuff

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,20 +1,81 @@
+//Validation Forms Throughout Site
+
 //Signup Page Form
-
-//Account Settings Edit Page Form
-
-//Profile Edit Page Form
-
-//Profile and Posts Comments Form
-if($("#comment-field").length) {
-	var comment_field = $("#comment-field").val();
-	$("#comment-submit").on('click', function() {
+$("#signup-submit").on('click', function() {
+	if ($(".signup-field").length) {
 		console.log("Clicked!");
-		$(".errors").append().empty();
-		if(comment_field === "" ) {
-			$(".errors").append("Please write a comment before submitting.");
+		var name_field = $("#name-field").val();
+		var email_field = $("#email-field").val();
+		var password_field = $("#password-field").val();
+		var password_confirmation_field = $("#password-confirmation-field").val();
+		if (name_field == "" || email_field == "" || password_field == "" ||password_confirmation_field == "") {
+			$(".errors").text("Please fill out all fields before submitting.");
 			return false;
 		} else {
 			return true;
-		}
-	});
-}
+		}	
+	}
+});
+
+//Login Page Form
+$("#login-submit").on('click', function() {
+	if ($(".login-field").length) {
+		console.log("Clicked!");
+		var email_field = $("#email-field").val();
+		var password_field = $("#password-field").val();
+		if (email_field == "" || password_field == "") {
+			$(".errors").text("Please fill out both your username and password.");
+			return false;
+		} else {
+			return true;
+		}	
+	}
+});
+
+//Account Settings Edit Page Form
+$("#account-settings-submit").on('click', function() {
+	if ($(".signup-field").length) {
+		console.log("Clicked!");
+		var name_field = $("#name-field").val();
+		var email_field = $("#email-field").val();
+		var password_field = $("#password-field").val();
+		var password_confirmation_field = $("#password-confirmation-field").val();
+		if (name_field == "" || email_field == "" || password_field == "" ||password_confirmation_field == "") {
+			$(".errors").text("Please fill out all fields before submitting.");
+			return false;
+		} else {
+			return true;
+		}	
+	}
+});
+
+//Profile Edit Page Form
+$("#profile-edit-submit").on('click', function() {
+	if ($(".profile-field").length) {
+		console.log("Clicked!");
+		var location_field = $("#location-field").val();
+		var occupation_field = $("#occupation-field").val();
+		var image_url_field = $("#image-url-field").val();
+		var about_me_field = $("#about-me-field").val();
+		if (location_field == "" || occupation_field == "" || image_url_field == "" || about_me_field == "") {
+			$(".errors").text("Please fill out all fields before submitting.");
+			return false;
+		} else {
+			return true;
+		}	
+	}
+});
+
+//Comment Form
+$("#comment-submit").on('click', function() {
+	if ($("#comment-field").length) {
+		console.log("Clicked!");
+		var comment_field = $("#comment-field").val();
+		if (comment_field == "") {
+			$(".errors").text("Please write a comment before submitting.");
+			return false;
+		} else {
+			return true;
+		}	
+	}
+});
